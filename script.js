97fb812e2980807d3b93937ca7b50ddc475a0721
faizan2307 <36506871+faizan2307@users.noreply.github.com>
2021-10-07 21:08:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var myApp=angular.module("myList",[]);
+myApp.controller("myListController",function($scope){
+    $scope.items=["AngularJS","ReactJS","VueJS"];
+    $scope.newItem="";
+    $scope.pushItem=function () {
+       if($scope.newItem != ""){ $scope.items.push($scope.newItem);
+        $scope.newItem= "";
+       }
+    }
+    $scope.deleteItem =function(index) {
+        $scope.items.splice(index,1);
+    }
+});
